b90d3abe838505f3d2dc49cf08b65ae33934b2ae
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-13 13:16:43 -0400

    starting a jQuery challenge from JD

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Make a request to the Star Wars API
+  // Get the first 10 starships
+// From the starship data you receive, output the X-Wing’s model name
+// and manufacturer to the DOM(Window)
+
+// Try making the request with fetch() and jQuery ($.get())
